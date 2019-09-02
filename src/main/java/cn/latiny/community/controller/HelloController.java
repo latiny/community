d388fe11810f4bef4b31e2ba73339d213830ea93
latiny <jinwei1003@163.com>
2019-09-02 09:47:47 +0800

    init repository

---
+++ src/main/java/cn/latiny/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package cn.latiny.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Latiny
+ * @version 1.0
+ * @description: HelloController
+ * @date 2019/8/30 14:53
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "/hello";
+    }
+}
